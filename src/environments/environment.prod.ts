85334ac5f064f2d1d7ba54f8364554a5405b9e35
Diego Juan Isaquiel <diego.mizael@facens.br>
2023-11-20 20:21:20 -0300

    build(environment):Adjust environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,27 @@
 export const environment = {
   production: true,
-  mapsKey: "AIzaSyDx61MV-11raK50UCiwJ6P6kdTflbV9oSE"
+  mapsKey: "AIzaSyDx61MV-11raK50UCiwJ6P6kdTflbV9oSE",
+  login: "https://login.salesforce.com/services/oauth2/token",
+  keys: {
+    token: 'TOKEN_PROXY_KEY',
+    refreshToken: 'REFRESH_TOKEN_PROXY_KEY',
+    user: 'USER_PROXY_KEY',
+  },
+  api: {
+    baseUrl: "https://facens-dev-ed.develop.my.salesforce.com/services/apexrest/cityconnect",
+    postagem: {
+      get: "/postagem/",
+      create: "/postagem/"
+    },
+    eventos: {
+      get: "/eventos/",
+    },
+    usuario: {
+      get: "/usuario/",
+      create: "/usuario/",
+    },
+    ranking: {
+      get: "/usuario/",
+    },
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,30 @@
 
 export const environment = {
   production: false,
-  mapsKey: "AIzaSyDx61MV-11raK50UCiwJ6P6kdTflbV9oSE"
+  mapsKey: "AIzaSyDx61MV-11raK50UCiwJ6P6kdTflbV9oSE",
+  login: "https://login.salesforce.com/services/oauth2/token",
+  keys: {
+    token: 'TOKEN_PROXY_KEY',
+    refreshToken: 'REFRESH_TOKEN_PROXY_KEY',
+    user: 'USER_PROXY_KEY',
+  },
+  api: {
+    baseUrl: "https://facens-dev-ed.develop.my.salesforce.com/services/apexrest/cityconnect",
+    postagem: {
+      get: "/postagem/",
+      create: "/postagem/"
+    },
+    eventos: {
+      get: "/eventos/",
+    },
+    usuario: {
+      get: "/usuario/",
+      create: "/usuario/",
+    },
+    ranking: {
+      get: "/usuario/",
+    },
+  }
 };
 
 /*
