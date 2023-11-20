e4cf67617a05137f0e2d6f78ac75ba18d2deb65e
Diego Juan Isaquiel <diego.mizael@facens.br>
2023-11-20 20:22:57 -0300

    feat(login): Create Form to Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  //#region Constructor
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+  ) {
+    this.formGroup = formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  //#endregion
+
+  //#region Public Properties
+
+  public formGroup!: FormGroup;
+
+  //#endregion
+
+  //#region Public Methods
+
+  public onSubmit(): void {
+
+
+    localStorage.setItem('ACCESS_TOKEN_PROXY_KEY', '123')
+  }
+
+  //#endregion
+}
